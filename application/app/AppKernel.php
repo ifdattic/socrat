ae048fcdb249746fd484727828699ef327aebd76
Andrew Marcinkevičius <andrew.web@ifdattic.com>
2016-04-17 15:58:09 +0300

    User can register and authenticate

--- application/app/AppKernel.php
+++ application/app/AppKernel.php
@@ -1,5 +1,6 @@
 <?php
 
+use FOS\UserBundle\FOSUserBundle;
 use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
 
@@ -16,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            new FOSUserBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
---
+++ application/src/AppBundle/Entity/User.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types = 1);
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="users")
+ */
+final class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
